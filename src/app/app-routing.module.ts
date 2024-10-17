ae24934d6ee545748d1a2ae8c59820df44088a53
Moksh Ajmera <ajmera.moksh002@gmail.com>
2024-10-18 01:03:55 +0530

    Moksh First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocListComponent } from './doc-list/doc-list.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"doc-list",component: DocListComponent},
+  {path:"home",component: HomeComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocListComponent } from './doc-list/doc-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
